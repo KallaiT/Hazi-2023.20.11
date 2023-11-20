adb2f0000a5a52f7ab0adcc058da0f714840c912
KallaiT <119456437+KallaiT@users.noreply.github.com>
2023-11-20 18:48:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const d  = document.querySelector('#gameid')
+
+for (let i = 0; i < 12; i++ )
+{
+    
+    const sambox = document.createElement('div')
+    sambox.innerHTML='a'
+    d.appendChild(sambox)
+}
